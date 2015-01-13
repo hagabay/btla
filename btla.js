27a8e71a3536aa523a09bec7b547959e98be7ad1
moti <hagabay@gmail.com>
2015-01-13 19:00:42 +0200

    2

--- btla.js
+++ btla.js
@@ -2,7 +2,7 @@ $( document ).ready(function() {
     // 
    // $this->response->setHeader('Access-Control-Allow-Origin', '*');
     $("#submit-phone").click(function(){
-        $("#login").hide();
+     //   $("#login").hide();
         
       $.ajax({
           dataType: 'jsonp',
@@ -10,8 +10,21 @@ $( document ).ready(function() {
           jsonp: 'jsonCallback',
           url: 'http://www.artech.org.il/index.php?option=com_btla_boss&tmpl=component&view=jobsitems&layout=users',
           success: function (response) {
-              $("#users").html(JSON.stringify(response, null, 2));
-          },
+              //$("#users").html(JSON.stringify(response, null, 2));
+            //  $.parseJSON(JSON.stringify(response, null, 2));
+               var obj = $.parseJSON( JSON.stringify(response, null, 2) );
+             // alert( obj.data );
+              $.each(obj.data, function(i, item) {
+                   var temp = '';
+                    $.each(item, function(j, val) {
+                        temp += 'data-'+j+'="'+val+'" ';
+                    });
+                     $("#users").append('<li><a href="http://www.artech.org.il/index.php?option=com_btla_boss&tmpl=component&" id="bosid-'+item.bosid+'" '+temp+'>'+item.username+"</li>");                
+                });
+          }
+          
+
+         
         });
           
       
